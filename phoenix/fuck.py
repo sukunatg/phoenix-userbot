d0cfb578dcd85702e85b712fb0d40ae48f8aa47e
Hacker <112378332+Hacker-UZ@users.noreply.github.com>
2023-09-05 22:57:12 +0500

    Delete phoenix directory

--- phoenix/fuck.py
+++
@@ -1,7 +0,0 @@
-from telethon import TelegramClient, events
-import phoenix.client
-client = phoenix.client.client
-
-@events.register(events.NewMessage(pattern=".fuck"))
-async def fuck(event):
-	await event.edit("┏━┳┳┳━┳┳┓\n┃━┫┃┃┏┫━┫┏┓\n┃┏┫┃┃┗┫┃┃┃┃\n┗┛┗━┻━┻┻┛┃┃\n┏┳┳━┳┳┳┓┏┫┣┳┓\n┣┓┃┃┃┃┣┫┃┏┻┻┫\n┃┃┃┃┃┃┃┃┣┻┫┃┃\n┗━┻━┻━┻┛┗━━━┛")
